c0a9f0958a53502406fa93cc44433eebc6908d20
DeniSaputra <denisaputrafebruari55@gmail.com>
2023-05-11 00:41:19 +0700

    uplod tugas update & delete tabel pelanggan,pesanan,kartu

--- admin/models/Pesanan.php
+++ admin/models/Pesanan.php
@@ -14,8 +14,7 @@ class Pesanan {
     }
 
     public function getPesanan($id){
-        $sql = "SELECT pesanan.*, pelanggan.nama_pelanggan as nama FROM pesanan INNER JOIN
-        pelanggan ON pelanggan.id = pesanan.pelanggan_id WHERE pesanan.id = ?";
+        $sql = "SELECT * FROM pesanan WHERE pesanan.id = ?";
         $ps = $this->koneksi->prepare($sql);
         $ps->execute([$id]);
         $rs = $ps->fetch();
@@ -28,6 +27,18 @@ class Pesanan {
         $ps = $this->koneksi->prepare($sql);
         $ps->execute($data);
     }
+
+    public function ubah($data){
+    $sql = "UPDATE pesanan SET tanggal=?, total=?, pelanggan_id=? WHERE id=?";
+    $ps = $this->koneksi->prepare($sql);
+    $ps->execute($data);
+}
+
+public function hapus($id){
+    $sql = "DELETE FROM pesanan WHERE id=?";
+    $ps = $this->koneksi->prepare($sql);
+    $ps->execute([$id]);
+}
 }
 
 ?>
